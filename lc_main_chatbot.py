6c5ee9e21a14552dccd94f481f9bec9114b05350
Rohit Sharma <nikrohit78@gmail.com>
2023-07-27 13:36:06 +0530

    changed key to rohit key

--- lc_main_chatbot.py
+++ lc_main_chatbot.py
@@ -20,7 +20,7 @@ if 'responses' not in st.session_state:
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
 
 if 'buffer_memory' not in st.session_state:
             st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
