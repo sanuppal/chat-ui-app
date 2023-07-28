e2875520add6a899869ddb0664b7df384cf8d8fd
Rohit Sharma <nikrohit78@gmail.com>
2023-07-28 11:41:56 +0530

    changed icons

--- lc_main_chatbot.py
+++ lc_main_chatbot.py
@@ -66,6 +66,6 @@ with response_container:
     if st.session_state['responses']:
 
         for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i],key=str(i))
+            message(st.session_state['responses'][i],key=str(i), avatar_style="identicon",seed="Aneka")
             if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user', avatar_style="bottts-neutral",seed="Oliver")
