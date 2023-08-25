4b0ef7a66f5fbe0479ce84a854dce63f65673c06
sanuppal <89642617+sanuppal@users.noreply.github.com>
2023-08-25 12:59:08 +0530

    Update lc_vector_search.py

--- lc_vector_search.py
+++ lc_vector_search.py
@@ -8,9 +8,13 @@ model = SentenceTransformer('all-MiniLM-L6-v2') #384 dimensional
 #pinecone.init(api_key="2d1b9675-5d6a-4a31-9a1f-7e1d7095b593", environment="us-west4-gcp-free")
 #index = pinecone.Index("penfed-data-index")
 
+### ************ Novartis Pinecone env details **********
+pinecone.init(api_key="312f1c16-1f6f-48ea-983a-f0a7b76a29ea", environment="gcp-starter")
+index = pinecone.Index("novartis-index")
+
 ##******velux index*********##
-pinecone.init(api_key="89c890aa-ead7-4c56-93d4-57968c3698c2", environment="asia-southeast1-gcp-free")
-index = pinecone.Index("velux-index-1")
+##pinecone.init(api_key="89c890aa-ead7-4c56-93d4-57968c3698c2", environment="asia-southeast1-gcp-free")
+##index = pinecone.Index("velux-index-1")
 
 ##
 # def addData(corpusData,url):
